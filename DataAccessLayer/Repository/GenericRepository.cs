a4809e48895f1038e7401959cf00e2f3f728ff6f
Aybuke Onay <aybuke.onay7@gmail.com>
2022-12-17 23:28:55 +0300

    Added with data access layer repository design pattern

---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T vrb_t)
+        {
+            using var c = new Context();
+            c.Remove(vrb_t);
+            c.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+        }
+
+        public void Insert(T vrb_t)
+        {
+            using var c = new Context();
+            c.Add(vrb_t);
+        }
+
+        public void Update(T vrb_t)
+        {
+            using var c = new Context();
+            c.Update(vrb_t);
+        }
+    }
+}
