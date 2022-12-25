c1f03213422f9e4cf045e721d7fb0fac03e9291d
Aybuke Onay <aybuke.onay7@gmail.com>
2022-12-25 22:41:27 +0300

    new thema added and partials edited

---
+++ TraversalCoreProject/ViewComponents/Default/_TestimonialPartial.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _TestimonialPartial : ViewComponent
+    {
+        TestimonialManager testimonial = new TestimonialManager(new EfTestimonialDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = testimonial.TGetAll();
+            return View(values);
+        }
+    }
+}
